0a26e2dbc46171fab09e875f15866e956100a76c
Valeriia Popova <valeriia.popova@nure.ua>
2020-04-02 23:50:21 +0300

    LESSON13

---
+++ Lesson13/Utils/TaskTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson13.Utils
+{
+    public enum TaskTypes
+    {
+        Task=1,
+        Bug=2,
+        TechDebt=3
+    }
+}
