8f15f8c53412ff7d4176e39f6f3413a1578ada22
Anna <frolenkova.a.a@gmail.com>
2020-04-05 11:45:10 +0300

    Lesson13

---
+++ Lesson13/Program.cs
@@ -0,0 +1,27 @@
+﻿using Lesson13.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task = Lesson13.Tasks.Task;
+
+namespace Lesson13
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            var story = new Story("Update Databas scheme");
+            story.BackLog.Add(new Bug("Invalid message", 10, 2, 60));
+            story.BackLog.Add(new Task("Add a column", 5, 1, 120));
+            story.BackLog.Add(new TechDebt("Remove exceed column", 1, 5, 60));
+
+            story.BackLog.ForEach(task => Console.WriteLine(task));
+
+            Console.WriteLine($"Total estimation of a story {story.Name} is {story.GetTotalEstimation()}");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson13/Utils/TaskType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13.Utils
+{
+    public enum TaskType
+    {
+        Task = 1,
+        Bug = 2,
+        TechDebt = 3
+
+    }
+}
